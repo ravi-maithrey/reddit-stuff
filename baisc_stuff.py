85b28b7a76487e0ea070421a48ab4eb9b9e18622
ravi maithrey <maitrey.ind@gmail.com>
2022-02-11 11:25:07 +0530

    from my pc

--- baisc_stuff.py
+++ baisc_stuff.py
@@ -1,10 +1,8 @@
 import praw
 
-#giving only the three below creates a read-only instance
-reddit = praw.Reddit(
-    client_id="WfBi7pJ4J9LwUA",
-    client_secret="ySGhjPD-jNXSwjwkp38jERqR6Jc",
-    user_agent="linux:com.example.scra:v0.1 (by u/dvskarna)"
-)
 
-print(reddit.read_only)
+#giving only the three below creates a read-only instance
+reddit = praw.Reddit("bot1")
+
+for submission in reddit.subreddit("learnprogramming").new(limit=4):
+    print(submission.title)
